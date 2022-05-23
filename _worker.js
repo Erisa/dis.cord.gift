e13d70b78c0bf99919a9e3c8bee9b2028c4486c3
Erisa A <erisa@erisa.uk>
2022-05-23 10:09:29 +0000

    Migrate to Cloudflare Pages

---
+++ _worker.js
@@ -0,0 +1,32 @@
+export default {
+    async fetch(request, env) {
+        const url = new URL(request.url);
+
+        const useragentIncludes = [
+            "Discordbot",
+            "aiohttp",
+            "python-httpx",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0"
+        ]
+
+        const useragent = request.headers.get("user-agent")
+        const match = useragentIncludes.find(element => {
+            if (useragent.includes(element)) {
+                return true;
+            }
+        }) !== undefined
+
+        if (url.pathname === "/images/ios.png") {
+            return env.ASSETS.fetch(request);
+        } else if (!match) {
+            return new Response(null, { status: 302, headers: { Location: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" } })
+        } else {
+            if (url.pathname === "/" || url.pathname === "/index" || url.pathname === "/index.html") {
+                return env.ASSETS.fetch(request);
+            } else {
+                url.pathname = "/embed"
+                return env.ASSETS.fetch(url);            
+            }
+        }
+    },
+};
