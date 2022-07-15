93a49f77fb2b19d87343cded487400696a4c3fd6
Erisa A <erisa@erisa.uk>
2022-07-15 18:53:51 +0000

    Add headers in the Worker

--- _worker.js
+++ _worker.js
@@ -9,6 +9,10 @@ export default {
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0"
         ]
 
+        const headers = {
+            "X-Notice": "Contact spotlight+rickroll@joscomputing.space for abuse or pure memes. No harm intended."
+        }
+
         const useragent = request.headers.get("user-agent")
         const match = useragentIncludes.find(element => {
             if (useragent.includes(element)) {
@@ -17,19 +21,26 @@ export default {
         }) !== undefined
 
         if (url.pathname === "/images/ios.png") {
-            return env.ASSETS.fetch(request);
+            return addHeaders(await env.ASSETS.fetch(request), headers);
         } else if (!match) {
-            return new Response(null, { status: 302, headers: { Location: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" } })
+            return addHeaders(new Response(null, { status: 302, headers: { Location: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" } }), headers)
         } else {
             if (url.pathname === "/" || url.pathname === "/index" || url.pathname === "/index.html") {
-                return env.ASSETS.fetch(request);
+                return addHeaders(await env.ASSETS.fetch(request), headers);
             } else {
                 url.pathname = "/embed"
                 const req = new Request(url.toString(), {
                     cf: request.cf
                 });
-                return env.ASSETS.fetch(req);            
+                return addHeaders(await env.ASSETS.fetch(req), headers);
             }
         }
     },
 };
+
+function addHeaders(response, headers) {
+    for (const property in headers) {
+        response.headers.set(property, headers[property])
+    }
+    return response
+}
