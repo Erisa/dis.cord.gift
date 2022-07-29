e0467bc251470c9ad58efb250cb32528ee7c0c24
Erisa A <erisa@erisa.uk>
2022-07-29 22:10:57 +0000

    Revert "Add headers in the Worker"
    This reverts commit 93a49f77fb2b19d87343cded487400696a4c3fd6.

--- _worker.js
+++ _worker.js
@@ -9,10 +9,6 @@ export default {
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0"
         ]
 
-        const headers = {
-            "X-Notice": "Contact spotlight+rickroll@joscomputing.space for abuse or pure memes. No harm intended."
-        }
-
         const useragent = request.headers.get("user-agent")
         const match = useragentIncludes.find(element => {
             if (useragent.includes(element)) {
@@ -21,26 +17,19 @@ export default {
         }) !== undefined
 
         if (url.pathname === "/images/ios.png") {
-            return addHeaders(await env.ASSETS.fetch(request), headers);
+            return env.ASSETS.fetch(request);
         } else if (!match) {
-            return addHeaders(new Response(null, { status: 302, headers: { Location: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" } }), headers)
+            return new Response(null, { status: 302, headers: { Location: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" } })
         } else {
             if (url.pathname === "/" || url.pathname === "/index" || url.pathname === "/index.html") {
-                return addHeaders(await env.ASSETS.fetch(request), headers);
+                return env.ASSETS.fetch(request);
             } else {
                 url.pathname = "/embed"
                 const req = new Request(url.toString(), {
                     cf: request.cf
                 });
-                return addHeaders(await env.ASSETS.fetch(req), headers);
+                return env.ASSETS.fetch(req);            
             }
         }
     },
 };
-
-function addHeaders(response, headers) {
-    for (const property in headers) {
-        response.headers.set(property, headers[property])
-    }
-    return response
-}
